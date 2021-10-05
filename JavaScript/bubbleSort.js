061c583269addeeb9f168e7d8a926ae849b8ec85
kazuki <matchingserver@gmail.com>
2021-10-05 19:08:09 +0900

    imprve js bubbleSort

--- JavaScript/bubbleSort.js
+++ JavaScript/bubbleSort.js
@@ -1,13 +1,12 @@
 'use strict';
 
 function bubbleSort(array) {
-  // 4
   const n = array.length - 1;
 
-  for(let end = n; end > 0; end--) { // end => 4.3,2,1
-    for(let i = 0; i < end; i++) { // i => 0,1,2,3,0,1,2,0,1,0
-      if (array[i] > array[i+1]) {
-        [array[i], array[i+1]] = [array[i+1], array[i]];
+  for (let end = n; end > 0; end--) {
+    for (let i = 0; i < end; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
       }
     }
   }
@@ -16,3 +15,19 @@ function bubbleSort(array) {
 const array = [7, 5, 10, 3, 4];
 bubbleSort(array);
 console.log(array);
+
+// const bubbleSort = (array) => {
+//   const n = array.length;
+//   for (let end = 0; end < n; end++) {
+//     for (let i = 0; i < n; i++) {
+//       if (array[i] > array[i + 1]) {
+//         [array[i], array[i + 1]] = [array[i + 1], array[i]]
+//       }
+//     }
+//   }
+// }
+// 
+// const array = [7, 5, 10, 3, 4];
+// bubbleSort(array);
+// console.log(array);
+
